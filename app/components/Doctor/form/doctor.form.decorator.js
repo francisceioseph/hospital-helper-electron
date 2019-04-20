9fb6285e4da8d395c235b7e1e73833e2d995681b
Francisco Souza <francisceioseph@gmail.com>
2019-04-20 18:58:27 -0300

    Update doctors service

--- app/components/Doctor/form/doctor.form.decorator.js
+++ app/components/Doctor/form/doctor.form.decorator.js
@@ -2,10 +2,10 @@ import { getRequiredDecorator, getOptionalDecorator, getDateTimeRequiredDecorato
 
 export function getDecoratorManager(decorator, values) {
   return {
-    birthDateDecorator            : getDateTimeRequiredDecorator('profile.personal_datum.full_name', decorator, values),
-    fullNameDecorator             : getRequiredDecorator('profile.personal_datum.cpf', decorator, values),
-    cpfDecorator                  : getRequiredDecorator('profile.personal_datum.gender', decorator, values),
-    genderDecorator               : getOptionalDecorator('profile.personal_datum.date_of_birth', decorator, values),
+    birthDateDecorator            : getDateTimeRequiredDecorator('profile.personal_datum.date_of_birth', decorator, values),
+    fullNameDecorator             : getRequiredDecorator('profile.personal_datum.full_name', decorator, values),
+    cpfDecorator                  : getRequiredDecorator('profile.personal_datum.cpf', decorator, values),
+    genderDecorator               : getOptionalDecorator('profile.personal_datum.gender', decorator, values),
     crmDecorator                  : getRequiredDecorator('profile.personal_datum.crm', decorator, values),
     emailDecorator                : getRequiredDecorator('email', decorator, values),
     passwordDecorator             : getRequiredDecorator('password', decorator, values),
--- app/pages/Doctors/doctors.service.js
+++ app/pages/Doctors/doctors.service.js
@@ -1,7 +1,13 @@
 import * as WebAPI from '../../utils/api.service';
 
 export const postDoctor = async (values) => {
-  const { user, ...doctor } = values;
+  const user = {
+    ...values,
+    profile: {
+      ...values.profile,
+      profile_type: 'Doctor',
+    },
+  };
+
   await WebAPI.postUser(user);
-  await WebAPI.createDoctor(doctor);
 };
