96572b4de4d117a0abde882ba5197d5e7a168974
Francisco Souza <francisco.souza@ifactory.com>
2019-05-09 12:19:38 -0300

    Add paciente registration modal to surgery page

--- app/pages/Surgeries/components/surgery.form.component.jsx
+++ app/pages/Surgeries/components/surgery.form.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
-import { Form, Button } from 'antd';
-import { compose, withHandlers, defaultProps } from 'recompose';
+import {
+  Form, Button, Row, Col
+} from 'antd';
+import {
+  compose, withHandlers, defaultProps, withState
+} from 'recompose';
 
 import { HORIZONTAL_FORM_LAYOUT, FORM_ITEM_LAYOUT, FORM_ITEM_SUBMIT_LAYOUT } from '../../../components/forms';
-
+import { PacientModalFormContainer } from '../../../containers/Pacient';
 import { getDecoratorManager } from './surgery.form.decorators';
 import { LABELS } from './surgery.form.constants';
 
@@ -19,6 +22,16 @@ import {
 
 const FormItem = Form.Item;
 
+const withModalVisible = withState('modalVisible', 'setModalVisible', false);
+
+const showNewPacientModal = props => () => {
+  props.setModalVisible(true);
+};
+
+const hideNewPacientModal = props => () => {
+  props.setModalVisible(false);
+};
+
 const handleSubmit = props => (e) => {
   e.preventDefault();
   props.form.validateFields((err, values) => {
@@ -28,7 +41,7 @@ const handleSubmit = props => (e) => {
   });
 };
 
-const withFormHandlers = withHandlers({ handleSubmit });
+const withFormHandlers = withHandlers({ handleSubmit, showNewPacientModal, hideNewPacientModal });
 
 const SurgeryForm = (props) => {
   const { surgery, form } = props;
@@ -39,9 +52,19 @@ const SurgeryForm = (props) => {
     <div>
       <Form onSubmit={props.handleSubmit} layout={HORIZONTAL_FORM_LAYOUT}>
         <FormItem label={LABELS.PACIENT_NAME} {...FORM_ITEM_LAYOUT} hasFeedback>
-          {decoratorManager.pacientNameDecorator(getPacientNameField(props.pacients))}
-          <Link to="/usuarios/pacientes/novo">Cadastrar Paciente</Link>
+          <Row gutter={8}>
+            <Col span={18}>{decoratorManager.pacientNameDecorator(getPacientNameField(props.pacients))}</Col>
+            <Col span={4}>
+              <Button onClick={props.showNewPacientModal}> Cadastrar Paciente </Button>
+            </Col>
+          </Row>
         </FormItem>
+
+        <PacientModalFormContainer
+          visible={props.modalVisible}
+          onCancel={props.hideNewPacientModal}
+          onSubmitSuccess={props.hideNewPacientModal}
+        />
 
         <FormItem label={LABELS.SURGERY_TYPE} {...FORM_ITEM_LAYOUT} hasFeedback>
           {decoratorManager.surgeryTypeDecorator(surgeryTypeField(props.surgeryTypes))}
@@ -70,6 +93,7 @@ const SurgeryForm = (props) => {
 };
 
 const SurgeryFormComponent = compose(
+  withModalVisible,
   defaultProps({
     surgery: {}
   }),
