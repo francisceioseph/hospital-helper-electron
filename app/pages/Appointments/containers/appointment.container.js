56462e07512b83927233da93c0b599c2895ec35f
Francisco Souza <francisco.souza@ifactory.com>
2019-05-16 13:21:36 -0300

    fixes

--- app/pages/Appointments/containers/appointment.container.js
+++ app/pages/Appointments/containers/appointment.container.js
@@ -1,31 +1,25 @@
-import _ from "lodash";
+import _ from 'lodash';
 
-import { connect } from "react-redux";
-import { withHandlers, compose, lifecycle } from "recompose";
+import { connect } from 'react-redux';
+import { withHandlers, compose, lifecycle } from 'recompose';
 
-import { getPacients } from "../../Pacient/pacient.actions";
-import { getDoctors } from "../../Doctors/doctors.actions";
-import {
-  createAppointment,
-  getAppointmentTypes
-} from "../appointments.actions";
+import { getPacients } from '../../Pacient/pacient.actions';
+import { getDoctors } from '../../Doctors/doctors.actions';
+import { createAppointment, getAppointmentTypes } from '../appointments.actions';
 
-import {
-  showPageLoader,
-  hidePageLoader
-} from "../../../containers/layouts/actions";
+import { showPageLoader, hidePageLoader } from '../../../containers/layouts/actions';
 
-import { AppointmentForm } from "../components";
+import { AppointmentForm } from '../components';
 
-import * as WebAPI from "../../../utils/api.service";
-import * as Alert from "../../../components/Alerts";
-import { printPdf } from "../../../utils/print-pdf";
+import * as WebAPI from '../../../utils/api.service';
+import * as Alert from '../../../components/Alerts';
+import { printPdf } from '../../../utils/print-pdf';
 
 const mapStateToProps = ({ appointments, doctors, pacients }) => ({
-  appointment: appointments.appointment,
-  appointmentTypes: _.values(appointments.appointmentTypes),
-  pacients: _.values(pacients.pacients),
-  doctors: _.values(doctors.doctors)
+  appointment      : appointments.appointment,
+  appointmentTypes : _.values(appointments.appointmentTypes),
+  pacients         : _.values(pacients.pacients),
+  doctors          : _.values(doctors.doctors)
 });
 
 const mapDispatchToProps = {
@@ -44,8 +38,8 @@ const showAppointmentPDF = async (appointment, form) => {
     form.resetFields();
   } catch (error) {
     Alert.error({
-      content: "Não foi possível acessar o arquivo PDF",
-      onOk: () => form.resetFields();
+      content : 'Não foi possível acessar o arquivo PDF',
+      onOk    : () => form.resetFields()
     });
   }
 };
@@ -56,16 +50,15 @@ const onAppointmentFormSubmit = props => async (values, form) => {
     props.createAppointment(appointment);
 
     Alert.confirm({
-      content:
-        "Agendamento realizado com sucesso. Deseja imprimir comprovante?",
-      okText: "Sim",
-      cancelText: "Não",
-      onOk: () => showAppointmentPDF(appointment, form),
-      onCancel: () => form.resetFields();
+      content    : 'Agendamento realizado com sucesso. Deseja imprimir comprovante?',
+      okText     : 'Sim',
+      cancelText : 'Não',
+      onOk       : () => showAppointmentPDF(appointment, form),
+      onCancel   : () => form.resetFields()
     });
   } catch (error) {
     Alert.error({
-      content: "Aconteceu um erro com o agendamento. Tente mais tarde!"
+      content: 'Aconteceu um erro com o agendamento. Tente mais tarde!'
     });
   }
 };
@@ -79,11 +72,7 @@ const withLifeCycle = lifecycle({
     try {
       this.props.showPageLoader();
 
-      const responses = await Promise.all([
-        WebAPI.getPacients(),
-        WebAPI.getDoctors(),
-        WebAPI.getAppointmentTypes()
-      ]);
+      const responses = await Promise.all([WebAPI.getPacients(), WebAPI.getDoctors(), WebAPI.getAppointmentTypes()]);
 
       this.props.getPacients(responses[0]);
       this.props.getDoctors(responses[1]);
@@ -93,7 +82,7 @@ const withLifeCycle = lifecycle({
     } catch (error) {
       this.props.hidePageLoader();
       Alert.error({
-        content: "Não foi possível carregar agora. Tente novamente mais tarde."
+        content: 'Não foi possível carregar agora. Tente novamente mais tarde.'
       });
     }
   }
